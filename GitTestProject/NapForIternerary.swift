3defd1bcaa03c306e3d4ad9211886b021114bc29
Jacques Parizeau <parizeauinnovations@gmail.com>
2020-02-29 11:15:52 -0500

    Add new nap for itinerary feature

---
+++ GitTestProject/NapForIternerary.swift
@@ -0,0 +1,13 @@
+//
+//  NapForIternerary.swift
+//  GitTestProject
+//
+//  Created by JJ on 2/29/20.
+//  Copyright © 2020 ParizeauInnovations. All rights reserved.
+//
+
+import UIKit
+
+class NapForIternerary: NSObject {
+
+}
