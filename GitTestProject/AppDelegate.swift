be52b69e6901109c75ded63f89b4032544717f8d
Jacques Parizeau <parizeauinnovations@gmail.com>
2020-02-29 11:09:22 -0500

    Log message on app launch Add button to UI

--- GitTestProject/AppDelegate.swift
+++ GitTestProject/AppDelegate.swift
@@ -14,8 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
         // Override point for customization after application launch.
+        print("Application did finish launching")
         return true
     }
 
